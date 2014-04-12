5d14a5a592cd0f8191fa58a323e9ec98c7a9b825
Paul Kang <sollipse@yahoo.com>
2014-04-12 16:22:21 -0700

    increasing permissions for app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1428635940721695', '0ee2514d630e0b3a1cdb8b1c2af9633a'
+  provider :facebook, '1428635940721695', '0ee2514d630e0b3a1cdb8b1c2af9633a', scope: "email,publish_stream"
 end
